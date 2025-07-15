5b28478e3e7873ca508a61060cdb9151bc23477c
Ekta Jangir <ekta210183@gmail.com>
2025-07-15 22:48:11 +0530

    added inngest server

--- ai_assistant_backend/inngest/client.js
+++ ai_assistant_backend/inngest/client.js
@@ -1,7 +1,7 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "ai_assistant" });
+export const inngest = new Inngest({ id: "ai_assistant" , eventKey: process.env.INNGEST_EVENT_KEY});
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [];
